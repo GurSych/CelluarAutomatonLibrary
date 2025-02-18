14ac920484ba44120dc280390efdf5ce9646cfa8
John Doe <dany.gurchin@gmail.com>
2025-02-18 22:25:47 +0300

    commit

--- CAL_Exceptions.hpp
+++ CAL_Exceptions.hpp
@@ -8,7 +8,7 @@ namespace gst {
         public:
             OutOfRange(const char* msg) : message{msg} {}
             const char* what() const throw() {
-                return ("CAL: Out of range error! " + message).c_str();
+                return (std::string{"CAL: Out of range error! "} + message).c_str();
             }
         private:
             std::string message{};
--- test.cpp
+++ test.cpp
@@ -2,8 +2,7 @@
 #include "CelluarAutomatonLibrary.hpp"
 
 int main() {
-    gtd::CelluarAutomaton<int,3,3> automat{};
-    std::cout << automat[0][0] << std::endl;
-    std::cout << automat[2][2] << std::endl;
+    gtd::CelluarAutomaton<int,4,7> automat{};
+    std::cout << automat[1][2] << std::endl;
     return 0;
 }
